e6b290120b9087d8946a2c01bf9f142e211628cd
doragi <wodls0125@gachon.ac.kr>
2023-09-14 23:46:52 +0900

    230914

---
+++ chap01/src/sec01/exam01/HelloJava.java
@@ -0,0 +1,12 @@
+package sec01.exam01;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello JAVA!");
+		// .은 참조 연산자 System 클래스의 out이라는 객체, out의 메서드(멤버함수) println을 실행
+		
+	
+	}
+
+}
